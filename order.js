5238c048d9950d1d22e3d4eb28f675c7af527cdc
Rob van den Hout <robvdhout@gmail.com>
2013-08-07 18:23:40 +0200

    Added remove item button.

--- order.js
+++ order.js
@@ -23,16 +23,51 @@ var Order = {
 			order_item.getElements('.TotalPrice, .SinglePrice').set('text', parseFloat(price, 10).toFixed(2));
 			order_item.getElements('.Count').set('text', 1);
 
+			order_item.getElements('.Remove').addEvent('click', function(){
+
+				Order.removeItem(id);
+			});
+
 			order_item.inject('order_item_dummy', 'before').show();
 		}
+
+		this.calculateTotal();
+	},
+
+	calculateTotal: function(){
 
 		var total = 0;
 
 		Object.keys(this.items).each(function(key){
 
-			total += (Order.items[key].count * Order.items[key].price);
+			if(Order.items[key]){
+
+				total += (Order.items[key].count * Order.items[key].price);
+			}
 		});
 
 		$('order_total').getElements('.Price').set('text', total.toFixed(2));
+	},
+
+	removeItem: function(id){
+
+		if(this.items[id]){
+
+			if(--this.items[id].count == 0){
+
+				this.items[id] = false;
+				$('order_item_' + id).destroy();
+			}
+			else{
+
+				$('order_item_' + id).getElements('.Count').set('text', this.items[id].count);
+				$('order_item_' + id).getElements('.TotalPrice').set(
+					'text',
+					parseFloat(this.items[id].count * this.items[id].price, 10).toFixed(2)
+				);
+			}
+		}
+
+		this.calculateTotal();
 	}
 };
