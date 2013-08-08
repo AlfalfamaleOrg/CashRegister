d89151ba02863a09277116bf663d2fbf82018169
Rob van den Hout <robvdhout@gmail.com>
2013-08-08 11:15:58 +0200

    dubbele div verwijderd

--- HTML/main/order.php
+++ HTML/main/order.php
@@ -1,23 +1,21 @@
 <div id="order">
 	<div id="order_list">
-		<div>
-			<table>
-				<tr>
-					<th class="Name">Artikel</th>
-					<th class="SinglePrice">P/s</th>
-					<th class="Count">Aantal</th>
-					<th class="TotalPrice">Totaal</th>
-					<th class="Remove_button"></th>
-				</tr>
-				<tr id="order_item_dummy" class="OrderItem">
-					<td class="Name"></td>
-					<td class="SinglePrice"></td>
-					<td class="Count"></td>
-					<td class="TotalPrice"></td>
-					<td class="Remove_button"><button class="Remove">-</button></td>
-				</tr>
-			</table>
-		</div>
+		<table>
+			<tr>
+				<th class="Name">Artikel</th>
+				<th class="SinglePrice">P/s</th>
+				<th class="Count">Aantal</th>
+				<th class="TotalPrice">Totaal</th>
+				<th class="Remove_button"></th>
+			</tr>
+			<tr id="order_item_dummy" class="OrderItem">
+				<td class="Name"></td>
+				<td class="SinglePrice"></td>
+				<td class="Count"></td>
+				<td class="TotalPrice"></td>
+				<td class="Remove_button"><button class="Remove">-</button></td>
+			</tr>
+		</table>
 	</div>
 	<div id="order_total">
 		Totaal: &euro; <span class="Price">0.00</span>
