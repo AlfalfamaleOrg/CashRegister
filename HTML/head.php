79e44f9e27618b74f87309d8a95c270c2213c52b
Rob van den Hout <robvdhout@gmail.com>
2013-08-08 11:56:39 +0200

    some more cleaning

--- HTML/head.php
+++ HTML/head.php
@@ -1,19 +1,11 @@
-<!-- Stylesheets -->
 <link rel="stylesheet/less" type="text/css" href="LESS/index.less">
 
-<!-- 3rd party scripts -->
-<script src="JS/3rdParty/less.js"></script>
-<script src="JS/3rdParty/mootools-core-1.4.5.js"></script>
-<script src="JS/3rdParty/mootools-more-1.4.0.1.js"></script>
-<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha3.js"></script>
+<?php
+	foreach(new DirectoryIterator('JS/3rdParty') as $file)
+		if($file->getExtension() == 'js')
+			echo "<script src=\"JS/3rdParty/$file\"></script>";
 
-<!-- Bootstrapper -->
-<script src="JS/bootstrap.js"></script>
-
-<!-- Scripts -->
-<script src="JS/api.js"></script>
-<script src="JS/user.js"></script>
-<script src="JS/main.js"></script>
-<script src="JS/order.js"></script>
-<script src="JS/payment.js"></script>
-<script src="JS/keypad.js"></script>
+	foreach(new DirectoryIterator('JS') as $file)
+		if($file->getExtension() == 'js')
+			echo "<script src=\"JS/$file\"></script>";
+?>
--- JS/bootstrap.js
+++ JS/bootstrap.js
@@ -3,8 +3,6 @@ window.addEvent('domready', function(){
 	Main.init();
 	Order.init();
 	Payment.init();
-
 	Keypad.init();
-
 	User.init();
 });
